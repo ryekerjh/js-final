98c069c824d872c270b2317286eb37b619d030bd
gidd45 <codyspang@yahoo.com>
2017-10-20 16:41:25 -0700

    started final project

---
+++ js/script.js
@@ -0,0 +1,5 @@
+function addToList(){
+  var add = document.createElement('li');
+  var listEvent = document.getElementById('list').innerHTML = document.tasks.event.value;
+  var listDate = document.getElementById('list').innerHTML = document.tasks.date.value;
+}
